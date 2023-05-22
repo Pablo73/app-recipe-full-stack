1ad2106f3668def72918f41864c730cc535cc651
Pablo Dominguez <pasaxo@gmail.com>
2023-05-22 17:02:23 -0300

    atualizado remoto

---
+++ back-end/app.ts
@@ -0,0 +1,24 @@
+import * as express from 'express';
+import errorMiddleware from './src/middlewares/errorMiddleware';
+
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.app.use(express.json());
+
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.use(errorMiddleware);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
+
+export const { app } = new App();
