2a3702429a654aa58816a920d4ea2a9de919a899
Pablo Dominguez <pasaxo@gmail.com>
2023-05-23 12:18:34 -0300

    criando exections

--- back-end/src/exections/HttpException.ts
+++ back-end/src/exections/HttpException.ts
@@ -1,10 +1,10 @@
 abstract class HttpException extends Error {
-    status: number;
-  
-    constructor(status: number, message: string) {
-      super(message);
-      this.status = status;
-    }
+  status: number;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
   }
-  
-  export default HttpException;
+}
+
+export default HttpException;
---
+++ back-end/src/exections/index.ts
@@ -0,0 +1,4 @@
+export { default as NotUnauthorizedException } from './Unauthorized';
+export { default as BadRequestException } from './BadRequest';
+export { default as NotFoundException } from './NotFound';
+export { default as UnprocessableEntity } from './UnprocessableEntity';
