e2c991334905d24a3da716d6bf6902a5aa3d2a5d
joaopassone <joao.passone@gmail.com>
2023-05-25 19:00:00 -0300

    Rotas de busca de comidas por nome e primeira letra

--- back-end/src/routes/mealsRouter.ts
+++ back-end/src/routes/mealsRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import CategoryController from '../controllers';
+import MealController from '../controllers/MealController';
 
 const mealsRouter = Router();
 const categoryController = new CategoryController();
+const mealController = new MealController();
 
 mealsRouter.get('/categories', categoryController.getCategory);
+mealsRouter.get('/search', mealController.searchMeals);
 
 export default mealsRouter;
